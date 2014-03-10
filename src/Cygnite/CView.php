a7fe2909e8ba9e7d861a29c9515cee1825e906ab
Sanjoy <sanjoy@root@localhost.com>
2014-03-10 21:45:49 +0530

    Doc comment changes on Cview page

--- src/Cygnite/CView.php
+++ src/Cygnite/CView.php
@@ -26,11 +26,9 @@ if (!defined('CF_SYSTEM')) {
  * @Description           :  This file is used to map all routing of the cygnite framework
  * @Author                :  Sanjoy Dey
  * @Copyright             :  Copyright (c) 2013 - 2014,
- * @Link	              :  http://www.cygniteframework.com
- * @Since	              :  Version 1.0
+ * @Link	          :  http://www.cygniteframework.com
+ * @Since	          :  Version 1.0
  * @Filesource
- * @Warning               :  Any changes in this library can cause abnormal behaviour of the framework
- *
  *
  */
 
