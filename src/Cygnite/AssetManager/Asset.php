784d7b4bb6341621805ffaf87e2350b1e6ceccf7
Sanjoy Dey <sanjoy09@hotmail.com>
2015-03-12 03:22:15 +0530

    Added new functionality to AssetCollection
    1.
    $asset->where('header')->add('style', array('path'=> ''));
    
    $asset->where('header')->dump('style));
    
    2.
     $asset->isExternal(true)->add(//..);
    
    3.
    $asset->add(//...)->add(//...)->combine();

--- src/Cygnite/AssetManager/Asset.php
+++ src/Cygnite/AssetManager/Asset.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the Cygnite package.
+ *
+ * (c) Sanjoy Dey <dey.sanjoy0@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Cygnite\AssetManager;
 
 use Cygnite\Proxy\StaticResolver;
@@ -9,35 +17,12 @@ use InvalidArgumentException;
 if (!defined('CF_SYSTEM')) {
     exit('External script access not allowed');
 }
-/**
- *  Cygnite Framework
- *
- *  An open source application development framework for PHP 5.3  or newer
- *
- *   License
- *
- *   This source file is subject to the MIT license that is bundled
- *   with this package in the file LICENSE.txt.
- *   http://www.cygniteframework.com/license.txt
- *   If you did not receive a copy of the license and are unable to
- *   obtain it through the world-wide-web, please send an email
- *   to sanjoy@hotmail.com so I can send you a copy immediately.
- *
- * @Package                    :  Packages
- * @Sub Packages               :  Helper
- * @Filename                   :  Assets
- * @Description                :  This helper is used to load all assets of Html page. Not implemented in current version. May be available on next version.
- * @Author                     :  Cygnite Dev Team
- * @Copyright                  :  Copyright (c) 2013 - 2014,
- * @Link                       :  http://www.cygniteframework.com
- * @Since                      :  Version 1.0
- * @Filesource
- * @Warning                    :  Any changes in this library can cause abnormal behaviour of the framework
- *
- *
- */
 
-/*
+/**
+ * Cygnite Asset Manager
+ *
+ * @author Sanjoy Dey <dey.sanjoy0@gmail.com>
+ * <code>
 $asset = AssetCollection::make(function($asset)
 {
     $asset->add('style', array('path' => 'css.cygnite.css', 'media' => '', 'title' => ''))
@@ -50,19 +35,89 @@ $asset = AssetCollection::make(function($asset)
 $asset->dump('style');
 $asset->dump('script');
 $asset->dump('link');
+ * </code>
 */
-
-
-
 
 class Asset extends StaticResolver implements \ArrayAccess
 {
-    protected $assets = array();
-
     private static $stylesheet = '<link rel="stylesheet" type="text/css"';
 
     private static $script = '<script type="text/javascript"';
 
+    private $where = 'default';
+
+    private $baseUrl;
+
+    public static $directoryName = 'assets';
+
+    private $assetDirectory;
+
+    private $external = false;
+
+    private $combine = false;
+
+    protected $assets = array();
+
+    private $paths = array();
+
+    /**
+     * Set where to group collection of assets together
+     * By tagging assets into key we can easily find out
+     * which collection user is requesting to
+     *
+     * Example:
+     *
+     * $asset->where('header')->add('style', array('path' => ''));
+     * $asset->where('footer')->add('style', array('path' => ''));
+     *
+     * $asset->where('header')->dump('style');
+     *
+     * @param $key
+     * @return $this
+     */
+    public function where($key)
+    {
+        $this->where = $key;
+
+        return $this;
+    }
+
+    /**
+     * We will check if external true,
+     * if so then we will set the base url as empty
+     * so that user can give his own path to load assets
+     *
+     * @param bool $flag
+     * @return $this
+     */
+    public function isExternal($flag = false)
+    {
+        $this->external = true;
+
+        if ($flag) {
+            $this->baseUrl = '';
+        }
+
+        return $this;
+    }
+
+    /**
+     *
+     */
+    public function getBaseUrl()
+    {
+        if ($this->external == false) {
+            return $this->baseUrl = Url::getBase();
+        }
+
+        return $this->baseUrl;
+    }
+
+    /**
+     * @param       $type
+     * @param array $arguments
+     * @return $this
+     */
     public function add($type, $arguments = array())
     {
         switch ($type) {
@@ -74,10 +129,15 @@ class Asset extends StaticResolver implements \ArrayAccess
                 break;
             case 'link':
                 call_user_func_array(array($this, $type), $arguments);
-                /*list($path, $name, $attributes) = array_values($arguments);
-                $this->{$type}($path, $name, $attributes);*/
                 break;
         }
+
+        return $this;
+    }
+
+    private function setLocation($path, $name)
+    {
+        $this->paths[$this->where][$name][] = $path;
 
         return $this;
     }
@@ -103,61 +163,103 @@ class Asset extends StaticResolver implements \ArrayAccess
     protected function style($href, $media = "", $title = "")
     {
         $media = (is_null($media)) ? 'media=all' : $media;
-        $title = (!is_null($title)) ? "title='$title'"  : '';
+        $title = (!is_null($title)) ? "title= '$title'"  : '';
+
+        $this->setLocation($href, strtolower(__FUNCTION__));
 
         if ( is_null($href) ) {
-             throw new InvalidArgumentException("Cannot pass null argument to ".__METHOD__);
+            throw new InvalidArgumentException("Style path cannot be null.");
         }
 
+        // Check if we regular expression exists
         if ($this->hasRegExp($href)) {
+            //We will include all style sheets from the given directory
+            $this->loadAssetsFromDir($href, $media, $title, 'style');
 
-            $stylePath= str_replace('\\', '/', $href);
-
-            $styles = glob($stylePath.'*');
-            foreach($styles as $style)
-            {
-                $this->assets[strtolower(__FUNCTION__)][] = (string)
-                    static::$stylesheet.' '.$media.'
-                    '.$title.' href="'.Url::getBase().$style.'" >'.PHP_EOL;
+            return $this->assets[$this->where][strtolower(__FUNCTION__)];
             }
-
-            return $this->assets[strtolower(__FUNCTION__)];
-        }
 
         list(, $caller) = debug_backtrace(false);
 
-        // if method called statically via facade accessor we
-        // will simply return the string
+        $styleTag = '';
+        $styleTag = static::$stylesheet.' '.$media.'
+                   '.$title.' href="'.$this->getBaseUrl().$href.'" >'.PHP_EOL;
+
+        /*
+         | If method called statically we will simply return
+         | string
+         */
         if ($this->isFacade($caller)) {
 
-            return $this->stripCarriage(static::$stylesheet.' '.$media.'
-        '.$title.' href="'.Url::getBase().$href.'" >'.PHP_EOL);
-
+            return $this->stripCarriage($styleTag);
         }
 
-        $this->assets[strtolower(__FUNCTION__)][] = (string)
-        static::$stylesheet.' '.$media.'
-        '.$title.' href="'.Url::getBase().$href.'" >'.PHP_EOL;
+        $this->assets[$this->where][strtolower(__FUNCTION__)][] = (string) $styleTag;
 
-        return $this->assets[strtolower(__FUNCTION__)];
+        return $this->assets[$this->where][strtolower(__FUNCTION__)];
+        }
+
+    /**
+     * Include all the stylesheets from the path
+     *
+     * @param $href
+     * @param $attr
+     * @param $title
+     * @param $type
+     */
+    private function loadAssetsFromDir($href, $attr, $title, $type = 'style')
+    {
+        $path= str_replace('\\', '/', $href);
+        $assets = glob($path.'*');
+
+        foreach($assets as $src) {
+            ($type == 'style') ? $this->setStyle($attr, $src, $title) : $this->setScript($src, $attr);
+        }
     }
 
+    /**
+     * We will set the styles into assets array
+     *
+     * @param $media
+     * @param $style
+     * @param $title
+     */
+    private function setStyle($media, $style, $title)
+    {
+        $this->assets[$this->where][strtolower(__FUNCTION__)][] = (string)
+        static::$stylesheet.' '.$media.'
+                    '.$title.' href="'.$this->getBaseUrl().$style.'" >'.PHP_EOL;
+    }
+
+    /**
+     * We will set the script into assets array
+     *
+     * @param $src
+     * @param $attributes
+     */
+    private function setScript($src, $attributes)
+    {
+        $this->assets[strtolower(__FUNCTION__)][] = (string)
+            static::$script.'
+                    src="'.Url::getBase().$src.'"'.$this->addAttributes($attributes).'></script>'.PHP_EOL;
+    }
+
+    /**
+     * @param $string
+     * @return bool
+     */
     private function hasRegExp($string)
     {
         return (strpos($string, '*') !== FALSE) ? true : false;
     }
 
+    /**
+     * @param $string
+     * @return string
+     */
     private function stripCarriage($string)
     {
         return trim(preg_replace('/\s\s+/', ' ', $string)).PHP_EOL;
-
-    }
-
-    public function dump($name)
-    {
-        foreach ($this->assets[$name] as $key => $value) {
-            echo $this->stripCarriage($value);
-        }
     }
 
     /**
@@ -179,50 +281,34 @@ class Asset extends StaticResolver implements \ArrayAccess
      */
     protected function script($url, $attributes = array())
     {
+        $this->setLocation($url, strtolower(__FUNCTION__));
 
+        // Check if regular expression exists
         if ($this->hasRegExp($url)) {
-
-            $scriptPath= str_replace('\\', '/', $url);
-
-            $scripts = glob($scriptPath.'*');
-            foreach($scripts as $script)
-            {
-                $this->assets[strtolower(__FUNCTION__)][] = (string)
-                    static::$script.'
-                    src="'.Url::getBase().$script.'"'.$this->addAttributes($attributes).'></script>'.PHP_EOL;
-            }
+            // Include all the assets from the directory
+            $this->loadAssetsFromDir($url, $attributes, '', 'script');
 
             return $this->assets[strtolower(__FUNCTION__)];
         }
 
         list(, $caller) = debug_backtrace(false);
 
+        $scriptTag = '';
+        $scriptTag = static::$script.'
+                src="'.$this->getBaseUrl().$url.'"'.$this->addAttributes($attributes).'></script>'.PHP_EOL;
+
+        /*
+        | If method called statically we will simply return
+        | as string
+        */
         if ($this->isFacade($caller)) {
 
-            return $this->stripCarriage(static::$script.'
-        src="'.Url::getBase().$url.'"'.$this->addAttributes($attributes).'></script>'.PHP_EOL);
-
+            return $this->stripCarriage($scriptTag);
         }
 
-
-        $this->assets[strtolower(__FUNCTION__)][] =
-        static::$script.'
-        src="'.Url::getBase().$url.'"'.$this->addAttributes($attributes).'></script>'.PHP_EOL;
+        $this->assets[strtolower(__FUNCTION__)][] = $scriptTag;
 
         return $this->assets[strtolower(__FUNCTION__)];
-    }
-
-
-
-    public function ajax($url, $data, $method, $callback, $callType = '')
-    {
-        $ajaxScript = '';
-        $ajaxScript = '<script type="text/javascript">';
-        $ajaxScript = '
-                                $.ajax();
-                                ';
-
-        $ajax_script = '</script>';
     }
 
     private function isFacade($caller)
@@ -244,19 +330,24 @@ class Asset extends StaticResolver implements \ArrayAccess
     protected function link($url, $name = null, $attributes = array())
     {
         $name =  (is_null($name)) ? $url :  $name;
+        $this->setLocation($url, strtolower(__FUNCTION__));
 
         list(, $caller) = debug_backtrace(false);
 
+        $lingTag = '';
+        $lingTag = '<a href="'.$this->getBaseUrl().Html::entities($url).'"
+         '.$this->addAttributes($attributes).'>'.Html::entities($name).'</a>'.PHP_EOL;
+
+        /*
+        | If method called statically we will simply return
+        | as string
+        */
         if ($this->isFacade($caller)) {
 
-            return $this->stripCarriage('<a href="'.Url::getBase().Html::entities($url).'"
-         '.$this->addAttributes($attributes).'>'.Html::entities($name).'</a>'.PHP_EOL);
-
+            return $this->stripCarriage($lingTag);
         }
 
-        $this->assets[strtolower(__FUNCTION__)][] =
-         '<a href="'.Url::getBase().Html::entities($url).'"
-         '.$this->addAttributes($attributes).'>'.Html::entities($name).'</a>'.PHP_EOL;
+        $this->assets[strtolower(__FUNCTION__)][] = $lingTag;
 
         return $this->assets[strtolower(__FUNCTION__)];
     }
@@ -281,6 +372,102 @@ class Asset extends StaticResolver implements \ArrayAccess
 
         return (count($html) > 0) ? ' '.implode(' ', $html) : '';
     }
+
+    /**
+     * We will dump assets to browser
+     *
+     * @param $name
+     */
+    public function dump($name)
+    {
+        //&& string_has('.', $name)
+        // Check {style.final} and display only combined asset into browser
+        if ($this->combine ) {
+            foreach ($this->paths[$this->where][$name] as $key => $value) {
+                echo $this->stripCarriage($value).PHP_EOL;
+            }
+        }
+
+        if (isset($this->assets[$this->where][$name])) {
+            foreach ($this->assets[$this->where][$name] as $key => $value) {
+                echo $this->stripCarriage($value).PHP_EOL;
+            }
+        }
+    }
+
+    /**
+     * @param $name
+     */
+    public function setAssetDir($name)
+    {
+        $this->assetDirectory = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAssetDirName()
+    {
+        return isset($this->assetDirectory) ? $this->assetDirectory : static::$directoryName;
+    }
+
+
+    /**
+     * We will combine all assets tagged to the given key
+     * and make a final file which will contain all asset
+     * source
+     *
+     * $asset->add('style', array('path' => ''))
+     *       ->add('style', array('path' => ''))
+     *       ->combine('style', 'final_css', 'assets/css/final.css');
+     *
+     * @param $name
+     * @param $path
+     * @param $file
+     * @throws AssetExistsException
+     */
+    public function combine($name, $path, $file)
+    {
+        $this->combine = true;
+
+        if (file_exists(CYGNITE_BASE.DS.$path.$file)) {
+            /*throw new AssetExistsException(
+                'File already exists in /'.$path.$file.'
+                 Please use different name to combine assets.'
+            );*/
+        }
+
+        $filePointer = fopen(CYGNITE_BASE.DS.$path.$file, "w") or die("Unable to open file!");
+
+        $content = " /* Import All Assets */ \n\n";
+        foreach ($this->paths[$this->where][$name] as $key => $src) {
+
+            if ($name == 'style') {
+                $content .= $this->combineStylesheets($content, $src, $file);
+            } else if ($name == 'script') {
+                $content .= $this->combineScripts();
+            }
+
+        }
+
+        fwrite($filePointer, $content);
+        fclose($filePointer);
+    }
+
+    private function combineStylesheets($content, $src, $file)
+    {
+        $assets = pathinfo($src);
+        $assetPath = str_replace($this->getAssetDirName(),'', $assets["dirname"]);
+        $content .= "@import '$assetPath$file'; \n";
+
+        return $content;
+    }
+
+    private function combineScripts()
+    {
+
+    }
+
 
     /**
      * (PHP 5 &gt;= 5.0.0)<br/>
