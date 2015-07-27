c86c609fc3a4ddfef35c5a897e442b03ad5a2a93
Sanjoy Dey <sanjoy09@hotmail.com>
2015-07-27 23:15:30 +0530

    Added Collection Class tests

---
+++ tests/Cygnite/Foundation/CollectionTest.php
@@ -0,0 +1,42 @@
+<?php
+use Mockery as m;
+use Cygnite\Foundation\Collection;
+
+class CollectionTest extends PHPUnit_Framework_TestCase
+{
+    private $data = [];
+
+    private $collection;
+    
+    public function setUp()
+    {
+        $this->data = ['foo' => 'Hello Foo', 'bar' => 'Hello Bar'];
+
+        $this->collection = new Collection($this->data);
+    }
+    
+    public function testCollectionInstance()
+    {
+        $collection = m::mock('Cygnite\Foundation\Collection');
+        $this->assertInstanceOf('Cygnite\Foundation\Collection', $collection);
+    }
+    
+    public function testGetDataCollection()
+    {
+        $this->assertEmpty(!$this->collection->getData());
+        $this->assertEquals($this->data, $this->collection->getData());
+    }
+    
+    public function testCollectionMethods()
+    {
+        $this->assertEquals($this->data, $this->collection->asArray());   
+        $this->assertEquals(json_encode($this->data), $this->collection->asJson());
+        $this->assertEquals(count($this->data), $this->collection->count());
+        $this->assertEquals(serialize($this->data), $this->collection->serialize());
+    }
+
+    public function tearDown()
+    {
+        m::close();
+    }   
+}
