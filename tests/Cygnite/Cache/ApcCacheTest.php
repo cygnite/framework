baccb70dea50791c5f35c082d05cba750332b139
Sanjoy Dey <sanjoy09@hotmail.com>
2015-10-02 16:54:42 +0530

    Tests refactoring done for passing tests

--- tests/Cygnite/Cache/ApcCacheTest.php
+++ tests/Cygnite/Cache/ApcCacheTest.php
@@ -4,23 +4,43 @@ use Cygnite\Cache\Factory\Cache;
 
 class ApcCacheTest extends PHPUnit_Framework_TestCase
 {
-    private $apc;
-
-    public function setUp()
+    public function testGetMethodReturnValue()
     {
-        $this->apc = Cache::make('apc', function ($apc) {
-            return $apc;
-        });
+        $apc = $this->getMock('Cygnite\Cache\Storage\ApcWrapper', ['get']);
+        $apc->expects($this->once())->method('get')->will($this->returnValue('foobar'));
+        $cache = new Cygnite\Cache\Storage\Apc($apc);
+        $this->assertEquals('foobar', $cache->get('foobar'));
     }
 
-    public function testInstance()
+    public function testGetMethodReturnsNullWhenItemNotFound()
     {
-        $this->assertInstanceOf('\Cygnite\Cache\Storage\Apc', $this->apc);
+        $apc = $this->getMock('Cygnite\Cache\Storage\ApcWrapper', ['get']);
+        $apc->expects($this->once())->method('get')->with($this->equalTo('foo'))->will($this->returnValue(null));
+        $cache = new Cygnite\Cache\Storage\Apc($apc);
+        $this->assertNull($cache->get('foo'));
     }
 
-    public function testStoreData()
+    public function testIncrementMethod()
     {
-        $this->apc->store('foo', 'Foo Bar');
-        $this->assertEquals('Foo Bar', $this->apc->get('foo'));
+        $apc = $this->getMock('Cygnite\Cache\Storage\ApcWrapper', ['increment']);
+        $apc->expects($this->once())->method('increment')->with($this->equalTo('bar'), $this->equalTo(10));
+        $cache = new Cygnite\Cache\Storage\Apc($apc);
+        $cache->increment('bar', 10);
+    }
+
+    public function testDecrementMethod()
+    {
+        $apc = $this->getMock('Cygnite\Cache\Storage\ApcWrapper', ['decrement']);
+        $apc->expects($this->once())->method('decrement')->with($this->equalTo('barbaz'), $this->equalTo(3));
+        $cache = new Cygnite\Cache\Storage\Apc($apc);
+        $cache->decrement('barbaz', 3);
+    }
+
+    public function testDestroyMethod()
+    {
+        $apc = $this->getMock('Cygnite\Cache\Storage\ApcWrapper', ['destroy']);
+        $apc->expects($this->once())->method('destroy')->with($this->equalTo('baz'));
+        $cache = new Cygnite\Cache\Storage\Apc($apc);
+        $cache->destroy('baz');
     }
 }
