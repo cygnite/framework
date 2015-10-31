8935b93833c9f24fc67b3a4aaa6e7179c4692736
Sanjoy Dey <sanjoy09@hotmail.com>
2015-11-01 04:05:12 +0530

    Added Connection tests

---
+++ tests/Cygnite/Database/ConnectionTest.php
@@ -0,0 +1,35 @@
+<?php
+use Cygnite\Database\Configure;
+use Cygnite\Database\ConnectionManagerTrait;
+
+class ConnectionTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        Configure::database(function ($config)
+        {
+            $config->default = 'db';
+            $config->set([
+                    'db' => [
+                        'driver' => 'mysql',
+                        'host' => 'localhostf',
+                        'port' => '',
+                        'database' => 'foo_bar',
+                        'username' => 'root',
+                        'password' => '',
+                        'charset'   => 'utf8',
+                        'collation' => 'utf8_unicode_ci',
+                    ]
+                ]);
+        });
+    }
+
+    public function testMySqlPdoConnection()
+    {
+        $connector = $this->getMock('Cygnite\Database\Connections\Mysql', ['create'], [Configure::$config['db']]);
+        $connector->expects($this->once())->method('create')->will($this->returnValue('PDO'));
+
+        $this->assertInstanceOf('Cygnite\Database\Connections\Mysql', $connector);
+        $this->assertEquals('PDO', $connector->create());
+    }
+}
