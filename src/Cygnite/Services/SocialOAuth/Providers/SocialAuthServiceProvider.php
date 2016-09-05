8d6367a411e6a8a99d83d8d76250c4e053e95e84
sanjoydesk <sanjoy09@hotmail.com>
2016-09-05 21:06:47 +0530

    Integrated PHPoAuthLib using SocialAuthServiceProvider

---
+++ src/Cygnite/Services/SocialOAuth/Providers/SocialAuthServiceProvider.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * This file is part of the Cygnite package.
+ *
+ * (c) Sanjoy Dey <dey.sanjoy0@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Cygnite\Services\SocialOAuth\Providers;
+
+use Cygnite\Helpers\Config;
+use OAuth\Common\Storage\Session;
+use Cygnite\Foundation\Application;
+use OAuth\Common\Consumer\Credentials;
+use Cygnite\Container\Service\ServiceProvider;
+
+/**
+ * Class SocialAuthServiceProvider
+ *
+ * @package Cygnite\Services\SocialOAuth\Providers
+ */
+class SocialAuthServiceProvider extends ServiceProvider
+{
+    protected $app;
+
+    private $config;
+
+    /**
+     * @param Application $app
+     */
+    public function register(Application $app)
+    {
+        if ($this->configureSocialAuth()) {
+            $this->registerSocialAuth();
+        }
+    }
+
+    /**
+     * Register the Social OAuth API class.
+     *
+     * @return void
+     */
+    protected function registerSocialAuth()
+    {
+        // Session storage
+        $storage = new Session();
+        $factory = new \OAuth\ServiceFactory();
+
+        foreach ($this->config['active'] as $key => $social) {
+
+            $this->app->singleton($social, function ($c) use($social, $factory, $storage) {
+                // Setup the credentials for the requests
+                $credentials = new Credentials(
+                    $this->config[$social]['key'],
+                    $this->config[$social]['secret'],
+                    $this->app->request->getFullUrl()
+                );
+
+                return $factory->createService($social, $credentials, $storage, []);
+            });
+        }
+    }
+
+    /**
+     * Set Stripe Configuration
+     *
+     * @return void
+     */
+    protected function configureSocialAuth()
+    {
+        $this->config = Config::get('global.config', 'social.config');
+
+        if (isset($this->config['active']) && !empty($this->config['active'])) {
+            return true;
+        }
+
+        return false;
+    }
+}
