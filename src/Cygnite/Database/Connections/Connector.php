b41122e3642592b4a1e7851be4f6cc6a5228af4e
sanjoydesk <sanjoy09@hotmail.com>
2016-12-25 14:49:58 +0530

    Cyrus ORM connection handled exceptions and reconnection

--- src/Cygnite/Database/Connections/Connector.php
+++ src/Cygnite/Database/Connections/Connector.php
@@ -39,10 +39,43 @@ class Connector
         $this->config = $config;
     }
 
-    /**
+     /**
+     * Return PDO connection object. If connection lost will try again to connect.
+     *
      * @return PDO
      */
     public function create()
+    {
+        $pdo = null;
+        try {
+            $pdo = $this->connect();
+        } catch (\Exception $e) {
+            $pdo = null;
+            if ($this->isLostConnection($e)) {
+                $pdo = $this->reconnect();
+            }
+
+            throw new \LogicException('Connection lost and no reconnector available.');
+        }
+
+        return $pdo;
+    }
+
+    /**
+     * Recreate connection object.
+     *
+     * @return PDO
+     */
+    public function reconnect()
+    {
+        return $this->connect();
+    }
+
+    /**
+     * Create connection
+     * @return PDO
+     */
+    public function connect()
     {
         return new PDO($this->getDsn(), $this->config['username'], $this->config['password'], $this->getOptions());
     }
@@ -77,5 +110,28 @@ class Connector
     public function getOptions()
     {
         return $this->options;
+    }
+    
+    /**
+     * Find if exception caused by lost connection.
+     *
+     * @param \Exception $e
+     * @return bool
+     */
+    protected function isLostConnection(\Exception $e)
+    {
+        return string_has(strtolower($e->getMessage()), [
+            'server has gone away',
+            'Lost connection',
+            'is dead or not enabled',
+            'Resource deadlock avoided',
+            'Error while sending',
+            'no connection to the server',
+            'decryption failed or bad record mac',
+            'Error writing data to the connection',
+            'Deadlock found when trying to get lock',
+            'server closed the connection unexpectedly',
+            'SSL connection has been closed unexpectedly',
+        ]);
     }
 }
