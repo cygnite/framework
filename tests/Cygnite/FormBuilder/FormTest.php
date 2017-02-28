a4290f6c5a28f6e77340f4ac5799880733754289
sanjoydesk <sanjoy09@hotmail.com>
2017-03-01 01:18:30 +0530

    Fixed Form builder tests

--- tests/Cygnite/FormBuilder/FormTest.php
+++ tests/Cygnite/FormBuilder/FormTest.php
@@ -1,45 +1,29 @@
 <?php
-
-use Cygnite\Base\Router\Router;
-use Cygnite\Common\Input;
+use PHPUnit\Framework\TestCase;
 use Cygnite\Common\UrlManager\Url;
 use Cygnite\FormBuilder\Form;
-use Cygnite\Foundation\Application;
+use Cygnite\Container\Container;
+use Cygnite\Tests\Container\ContainerDependency;
 use Cygnite\Helpers\Config;
 
-class FormTest extends PHPUnit_Framework_TestCase
+class FormTest extends TestCase
 {
-    private $app;
+    private $container;
 
-    public function testCreateFormInstance()
+    public function setUp()
     {
-        $form = Form::make();
-        $this->assertInstanceOf('Cygnite\FormBuilder\Form', $form);
-
-        $formInstance = Form::instance();
-        $this->assertInstanceOf('Cygnite\FormBuilder\Form', $formInstance);
-    }
-
-    public function testCreateFormClouserInstance()
-    {
-        $form = Form::make(function ($form) {
-            return $form;
-        });
-
-        $this->assertInstanceOf('Cygnite\FormBuilder\Form', $form);
-    }
-
-    private function setUpAssetConfig()
-    {
-        $this->app = Application::instance();
-        $this->app['url'] = new \Cygnite\Common\UrlManager\Url();
-        $this->app['request'] = \Cygnite\Http\Requests\Request::createFromGlobals();
-        $this->app['router'] = new \Cygnite\Base\Router\Router($this->app['request']);
-        $this->app['router']->setApplication($this->app);
-        $this->app['url']->setApplication($this->app);
-
-        $this->app['request']->server->add('REQUEST_URI', '/hello/user/');
-        $this->app['request']->server->add('HTTP_HOST', 'localhost');
+        $containerDependency = new ContainerDependency();
+        $this->container = new Container(
+            $containerDependency->getInjector(),
+            $containerDependency->getDefinitiions(),
+            $containerDependency->getControllerNamespace()
+        );
+        $this->container['request'] = \Cygnite\Http\Requests\Request::createFromGlobals();
+        $this->container['router'] = $this->container->make(\Cygnite\Router\Router::class);
+        $this->container['router']->setContainer($this->container);
+        $this->container['url'] = new \Cygnite\Common\UrlManager\Url(new Cygnite\Common\UrlManager\Manager($this->container));
+        $this->container['request']->server->add('REQUEST_URI', '/hello/user/');
+        $this->container['request']->server->add('HTTP_HOST', 'localhost');
         $configuration = [
             'global.config' => [
                 'encoding' => 'utf-8',
@@ -51,10 +35,23 @@ class FormTest extends PHPUnit_Framework_TestCase
         Url::setBase('/cygnite/'); //$app['router']->getBaseUrl()
     }
 
+    public function testCreateFormInstance()
+    {
+        $form = Form::make();
+        $this->assertInstanceOf('Cygnite\FormBuilder\Form', $form);
+    }
+
+    public function testCreateFormClouserInstance()
+    {
+        $form = Form::make(function ($form) {
+            return $form;
+        });
+
+        $this->assertInstanceOf('Cygnite\FormBuilder\Form', $form);
+    }
+
     public function testCreateFormLabelAndTextBox()
     {
-        $this->setUpAssetConfig();
-
         $form = Form::make()
                     ->open(
                         'contact',
@@ -62,12 +59,6 @@ class FormTest extends PHPUnit_Framework_TestCase
                             'method' => 'post',
                             'action' => Url::sitePath('contact/add'),
                             'role'   => 'form',
-                            'style'  => 'width:500px;margin-top:35px;float:left;',
-                        ]
-                    )->addElement('label', 'User Name',
-                        [
-                            'class' => 'col-sm-2 control-label',
-                            'style' => 'width:37.667%;',
                         ]
                     )->addElement('text', 'user_name',
                         [
@@ -78,18 +69,15 @@ class FormTest extends PHPUnit_Framework_TestCase
                     ->close()
                     ->createForm();
 
-        $formString = preg_replace("/\r\n|\r|\n|\t/", '', trim("<form name='contact' method='post' action='http://localhost/cygnite/contact/add' role='form' style='width:500px;margin-top:35px;float:left;' >
-					<label for='User Name' class='col-sm-2 control-label' style='width:37.667%;' >User Name</label>
-					<input name='user_name' value='' class='form-control' type='text'  />
-					</form>"));
-
-        $this->assertEquals($formString, preg_replace("/\r\n|\r|\n|\t/", '', trim($form->getForm())));
+        $formString = "<form name='contact' method='post' action='http://localhost/cygnite/contact/add' role='form' >
+                    <input name='user_name' value='' class='form-control' type='text'  />
+                    </form>";
+        $this->assertEquals(preg_replace("/\r\n|\r|\n|\t/", '', trim(preg_replace('/\s+/',' ', $formString))), preg_replace("/\r\n|\r|\n|\t/", '', trim(preg_replace('/\s+/',' ',$form->getForm()))));
     }
+
 
     public function testCreateFormTextAreaAndFileAndCheckBoxAndRadioElements()
     {
-        $this->setUpAssetConfig();
-
         $form = Form::make()
                     ->open(
                         'contact',
@@ -128,22 +116,20 @@ class FormTest extends PHPUnit_Framework_TestCase
                     ->close()
                     ->createForm();
 
-        $formString = preg_replace("/\r\n|\r|\n|\t/", '', trim("<form name='contact' method='post' action='http://localhost/cygnite/contact/add' role='form' style='width:500px;margin-top:35px;float:left;' >
-						<label for='User Name' class='col-sm-2 control-label' style='width:37.667%;' >User Name</label>
-						<textarea for='description' class='form-control' ></textarea>
-						<input name='photo' value='' class='form-control' type='file'  />
-						<input name='Useful' value='' class='form-control' type='checkbox'  />
-						<input name='Male' value='' class='form-control' type='radio'  />
-						</form>"));
+        $formString = preg_replace("/\r\n|\r|\n|\t/", '', trim(preg_replace('/\s+/',' ', "<form name='contact' method='post' action='http://localhost/cygnite/contact/add' role='form' style='width:500px;margin-top:35px;float:left;' >
+                        <label for='User Name' class='col-sm-2 control-label' style='width:37.667%;' >User Name</label>
+                        <textarea for='description' class='form-control' ></textarea>
+                        <input name='photo' value='' class='form-control' type='file'  />
+                        <input name='Useful' value='' class='form-control' type='checkbox'  />
+                        <input name='Male' value='' class='form-control' type='radio'  />
+                        </form>")));
 
-        $this->assertEquals($formString, preg_replace("/\r\n|\r|\n|\t/", '', trim($form->getForm())));
+        $this->assertEquals($formString, preg_replace("/\r\n|\r|\n|\t/", '', trim(preg_replace('/\s+/',' ', $form->getForm()))));
     }
 
     public function testCustomTagsByFormBuilder()
     {
-        $this->setUpAssetConfig();
-
-        $form = Form::make()
+          $form = Form::make()
                     ->open(
                         'contact',
                         [
@@ -161,17 +147,15 @@ class FormTest extends PHPUnit_Framework_TestCase
                     )->createForm()
                     ->close();
 
-        $formString = preg_replace("/\r\n|\r|\n|\t/", '', trim("<form name='contact' method='post' action='http://localhost/cygnite/contact/add' role='form' style='width:500px;margin-top:35px;float:left;' >
-					<dl for='dl' name='Custom Tag' class='col-sm-2 control-label' style='width:37.667%;' >Custom Tag</dl>
-					</form>"));
+        $formString = preg_replace("/\r\n|\r|\n|\t/", '', trim(preg_replace('/\s+/',' ', "<form name='contact' method='post' action='http://localhost/cygnite/contact/add' role='form' style='width:500px;margin-top:35px;float:left;' >
+                    <dl for='dl' name='Custom Tag' class='col-sm-2 control-label' style='width:37.667%;' >Custom Tag</dl>
+                    </form>")));
 
-        $this->assertEquals($formString, preg_replace("/\r\n|\r|\n|\t/", '', trim($form->getForm())));
+        $this->assertEquals($formString, preg_replace("/\r\n|\r|\n|\t/", '', trim(preg_replace('/\s+/',' ', $form->getForm()))));
     }
 
     public function testDivOrSpanElementForFormBuilder()
     {
-        $this->setUpAssetConfig();
-
         $form = Form::make()
                     ->open(
                         'contact',
@@ -194,13 +178,13 @@ class FormTest extends PHPUnit_Framework_TestCase
                     ->close();
 
         $formString = preg_replace("/\r\n|\r|\n|\t/", '', trim("<form name='contact' method='post' action='http://localhost/cygnite/contact/add' role='form' style='width:500px;margin-top:35px;float:left;' >
-			<div name='div_1_1331' style='border:1px solid red;height:40px;'  />
-			<input name='I am Inside Div 1' class='col-sm-2 control-label' style='width:100%;' type='text'  />
-			</div>
-			<div name='div_2_1224' style='border:1px solid red;height:40px;'  />
-			<input name='I am Inside Div 2' class='col-sm-2 control-label' style='width:100%;' type='text'  />
-			</div>
-			</form>"));
+            <div name='div_1_1331' style='border:1px solid red;height:40px;'  />
+            <input name='I am Inside Div 1' class='col-sm-2 control-label' style='width:100%;' type='text'  />
+            </div>
+            <div name='div_2_1224' style='border:1px solid red;height:40px;'  />
+            <input name='I am Inside Div 2' class='col-sm-2 control-label' style='width:100%;' type='text'  />
+            </div>
+            </form>"));
 
         // It works though But we cannot Test it because it generate random name for Div or Span Element
         // For Example [name='div_2_1224' or name='div_1_1331']
