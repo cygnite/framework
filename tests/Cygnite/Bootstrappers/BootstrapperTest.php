876ba670d1dc8a7e956849220d80c6f00a8b7371
sanjoydesk <sanjoy09@hotmail.com>
2017-03-01 01:10:43 +0530

    Added Bootstrapper component tests

---
+++ tests/Cygnite/Bootstrappers/BootstrapperTest.php
@@ -0,0 +1,38 @@
+<?php
+use Cygnite\Bootstrappers\Paths;
+use Cygnite\Bootstrappers\Bootstrapper;
+use PHPUnit\Framework\TestCase;
+
+class BootstrapperTest extends TestCase
+{
+    public function testReturnsDefaultFrameworkBootstrappers()
+    {
+        $bootstrapper = new Bootstrapper(new \Cygnite\Bootstrappers\Paths([]));
+        $this->assertEquals([
+            \Cygnite\Foundation\Bootstrappers\ApplicationBootstraper::class,
+            \Cygnite\Foundation\Bootstrappers\LogBootstraper::class,
+        ], $bootstrapper->all());
+    }
+
+    public function testAddingAdditionalBootstrappers()
+    {
+        $bootstrapper = new Bootstrapper(new \Cygnite\Bootstrappers\Paths([]));
+        $bootstrapper->registerBootstrappers([
+            \Cygnite\Tests\Bootstrappers\Executes\AppBootstraper::class
+        ]);
+
+        $this->assertEquals([
+            \Cygnite\Foundation\Bootstrappers\ApplicationBootstraper::class,
+            \Cygnite\Foundation\Bootstrappers\LogBootstraper::class,
+            \Cygnite\Tests\Bootstrappers\Executes\AppBootstraper::class
+        ], $bootstrapper->all());
+    }
+
+    public function testReturnsPathsObject()
+    {
+        $bootstrapper = new Bootstrapper(new \Cygnite\Bootstrappers\Paths([]));
+        $this->assertInstanceof(\Cygnite\Bootstrappers\Paths::class, $bootstrapper->getPaths());
+    }
+}
+
+
